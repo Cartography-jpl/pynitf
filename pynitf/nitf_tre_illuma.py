5ccce69ff17d343f63ad467c32f314084f5d5759
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2018-11-13 11:04:52 -0800

    Initial implementation of ILLUMA TRE. Not done yet

---
+++ pynitf/nitf_tre_illuma.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+from .nitf_field import *
+from .nitf_tre import *
+import six
+
+hlp = '''This is the ILLUMA TRE, Illumination for Spectral Products. 
+
+The field names can be pretty cryptic, but are documented in detail in 
+the NGA's SNIP documentation. It is current in draft and is subject to change.
+
+The SNIP documentation is currently not available to the public.
+
+!!! Note that in the current version of ILLUMA you can choose between using traditional fields and using a single
+!!! XML file. The conditional flag in choosing between the two is the value of the CEL field in the TRE subheader.
+!!! This practice is highly unorthodox and our current pynitf design doesn't easily support this.
+!!! For now we will only support the traditional way of using this TRE. Once this TRE is finalized and ratified
+!!! by the NTB, we can implement the final version.
+'''
+
+desc = ["ILLUMA",
+        ["sol_az", "Sun Azimuth Angle", 5, float, {'frmt': '%05.1f'}],
+        ["sol_el", "Sun Elevation Angle", 5, float, {'frmt': '%+04.1f'}],
+        ["com_sol_il", "Computed Solar Illumination", 5, float, {'frmt': '%05.1f'}],
+        ["lun_el", "Lunar Elevation Angle", 5, float, {'frmt': '%05.1f'}],
+        ["lun_ph_ang", "Phase Angle of the Moon in Degrees", 6, float, {'frmt': '%+05.1f'}],
+        ["lun_az", "Lunar Azimuth Angle", 5, float, {'frmt': '%05.1f'}],
+        ["com_lun_il", "Computed Lunar Illumination", 5, float, {'frmt': '%05.1f'}],
+        ["com_tot_nat_il", "Computed Total Natural Illumination", 5, float, {'frmt': '%05.1f'}],
+        ["sol_lun_dis_ad", "Solar/Lunar Distance Adjustment", 3, float, {'frmt': '%03.1f'}],
+        ["art_ill_min", "Minimum Artificial Illumination", 5, float, {'frmt': '%05.1f'}],
+        ["art_ill_max", "Maximum Artificial Illumination", 5, float, {'frmt': '%05.1f'}],
+]
+
+TreILLUMA = create_nitf_tre_structure("TreILLUMA",desc,hlp=hlp)
+
---
+++ tests/nitf_tre_illuma_test.py
@@ -0,0 +1,33 @@
+from pynitf.nitf_tre import *
+from pynitf.nitf_tre_illuma import *
+from pynitf_test_support import *
+import io, six
+
+@pytest.mark.skip
+def test_tre_illuma_basic():
+
+    t = TreILLUMA()
+
+    # Set some values
+    t.sol_az = 180.1
+    t.sol_el = -90.0
+    t.com_sol_il = 555.6
+    t.lun_el = -45.0
+    t.lun_ph_ang = +180.0
+    t.lun_az = 180.1
+    t.com_lun_il = 555.6
+    t.com_tot_nat_il = 555.6
+    t.sol_lun_dis_ad = 1.1
+    t.art_ill_min = '-----'
+
+    fh = six.BytesIO()
+    t.write_to_file(fh)
+    print(fh.getvalue())
+    assert fh.getvalue() == b'CSCRNA00109Y-90.00000-170.00000+10555.5+09.00000-170.00000+00000.0+90.00000+170.00000+00004.5-09.00000+017.00000-00555.5'
+
+    fh2 = six.BytesIO(fh.getvalue())
+    t2 = TreILLUMA()
+    t2.read_from_file(fh2)
+    assert t2.predict_corners == "Y"
+
+    print (t2.summary())
