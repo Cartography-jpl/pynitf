bb7672743145136ee5521cdb200554577cb6f19f
Walt Bunch <walter.l.bunch@jpl.nasa.gov>
2019-05-24 00:29:06 +0000

    added tre mimcsa and test

---
+++ pynitf/nitf_tre_mimcsa.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+from .nitf_field import *
+from .nitf_tre import *
+import six
+
+hlp = '''This is the MIMCSA TRE, History version A.
+
+The field names can be pretty cryptic, but are documented in detail in
+the NITF TRE documentation (NGA.STND.0044_1.3_MIE4NITF Adjudicated v3.pdf), 
+available at https://nsgreg.nga.mil/doc/view?i=4754
+
+There is a table in the main body on page vii that gives the a pointer for
+where in the document a particular TRE is defined.
+
+MIMCSA is documented at L-10.
+'''
+desc = ["MIMCSA",
+        ["layer_id", "Layer ID", 36, str],
+        ["nominal_frame_rate", "Nominal Frame Rate", 13, float],
+        ["min_frame_rate", "Minimum Frame Rate", 13, float],
+        ["max_frame_rate", "Maximum Frame Rate", 13, float],
+        ["t_rset", "Temporal Resolution Set", 2, int],
+        ["mi_req_decoder", "Image Compression", 2, str, {'default' : 'NC'}],
+        ["mi_req_profile", "Compression Profile", 36, str],
+        ["mi_req_level", "Compression Level", 6, str],
+]
+
+TreMIMCSA = create_nitf_tre_structure("TreMIMCSA",desc,hlp=hlp)
+
+def _summary(self):
+    res = six.StringIO()
+    print("MIMCSA:", file=res)
+    print("Layer ID: %s" % (self.layer_id), file=res)
+    print("Nominal Frame Rate: %f" % (self.nominal_frame_rate), file=res)
+    print("Minimum Frame Rate: %f" % (self.min_frame_rate), file=res)
+    print("Maximum Frame Rate: %f" % (self.max_frame_rate), file=res)
+    print("Temporal Resolution Set: %d" % (self.t_rset), file=res)
+    print("Image Compression: %s" % (self.mi_req_decoder), file=res)
+    print("Compression Profile: %s" % (self.mi_req_profile), file=res)
+    print("Compression Level: %s" % (self.mi_req_level), file=res)
+    return res.getvalue()
+
+TreMIMCSA.summary = _summary
+
+__all__ = [ "TreMIMCSA" ]
---
+++ tests/nitf_tre_mimcsa_test.py
@@ -0,0 +1,25 @@
+from pynitf.nitf_tre import *
+from pynitf.nitf_tre_mimcsa import *
+from pynitf_test_support import *
+import io, six
+    
+def test_tre_mimcsa_basic():
+
+    t = TreMIMCSA()
+
+    #Set some values
+    t.layer_id = "Some layer ID"
+    t.nominal_frame_rate = 22.2
+    t.min_frame_rate = 10.1
+    t.max_frame_rate = 29.9
+    t.t_rset = 0
+    t.mi_req_decoder = "NC"
+    t.mi_req_profile = "N/A"
+    t.mi_req_level = "N/A"
+
+    print (t.summary())
+
+    fh = six.BytesIO()
+    t.write_to_file(fh)
+    print(fh.getvalue())
+    assert fh.getvalue() == b'MIMCSA00121Some layer ID                       22.2         10.1         29.9         00NCN/A                                 N/A   '
