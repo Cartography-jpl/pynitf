ed7b3a9e74acbf09bb5da8c884a45e0cef2f7db8
Walt Bunch <walter.l.bunch@jpl.nasa.gov>
2019-05-29 21:46:24 +0000

    added micida tre

---
+++ pynitf/nitf_tre_micida.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+from .nitf_field import *
+from .nitf_tre import *
+import six
+
+hlp = '''This is the MICIDA TRE, Motion Imagery Core Identification
+
+The field names can be pretty cryptic, but are documented in detail in
+the NITF TRE documentation (NGA.STND.0044_1.3_MIE4NITF Adjudicated v3.pdf), 
+available at https://nsgreg.nga.mil/doc/view?i=4754
+
+There is a table in the main body on page vii that gives the a pointer for
+where in the document a particular TRE is defined.
+
+MICIDA is documented at in Table 13.
+'''
+desc = ["MICIDA",
+        ["miis_core_id_version", "MISB ST 1204 Version Number", 2, int],
+        ["num_camera_ids", "Number of MIIS Core Identifiers", 3, int],
+        [["loop", "f.num_camera_ids"],
+         ["cameras_id", "Camera UUID", 36, str],
+         ["core_id_length", "MIIS Core Identifier Length", 3, int],
+         ["camera_core_id", "MIIS Core Identifier", "f.core_id_length[i1]", None,
+          {'field_value_class' : StringFieldData}],
+        ],
+]
+
+TreMICIDA = create_nitf_tre_structure("TreMICIDA",desc,hlp=hlp)
+TreMICIDA.miis_core_id_version = hardcoded_value(1)
+
+def _summary(self):
+    res = six.StringIO()
+    print("MICIDA:", file=res)
+    print("MISB ST 1204 Version Number: %02d" % (self.miis_core_id_version()), file=res)
+    print("Number of MIIS Core Identifiers: %d" % (self.num_camera_ids), file=res)
+    for i in range(self.num_camera_ids):
+        print("Camera UUID: %s" % (self.cameras_id[i]), file=res)
+        print("MIIS Core Identifier Length: %d" % (self.core_id_length[i]), file=res)
+        print("MIIS Core Identifier: %s" % (self.camera_core_id[i]), file=res)
+
+    return res.getvalue()
+
+TreMICIDA.summary = _summary
+
+__all__ = [ "TreMICIDA" ]
---
+++ tests/nitf_tre_micida_test.py
@@ -0,0 +1,29 @@
+from pynitf.nitf_tre import *
+from pynitf.nitf_tre_micida import *
+from pynitf_test_support import *
+import io, six
+    
+def test_tre_micida_basic():
+
+    t = TreMICIDA()
+
+    #Set some values
+    t.num_camera_ids = 2
+
+    t.cameras_id[0] = "First camera ID"
+    core_id = b"First core ID"
+    t.core_id_length[0] = len(core_id)
+    t.camera_core_id[0] = core_id
+
+    t.cameras_id[1] = "Second camera ID"
+    core_id = b"Second core ID"
+    t.core_id_length[1] = len(core_id)
+    t.camera_core_id[1] = core_id
+
+    print (t.summary())
+
+    fh = six.BytesIO()
+    t.write_to_file(fh)
+    print(fh.getvalue())
+    assert fh.getvalue() == b'MICIDA0011000002First camera ID                     013First core IDSecond camera ID                    014Second core ID'
+    
