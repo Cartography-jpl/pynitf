9b83e7b20428c3ac7bbdfb44ddbd4c30fa1c7a99
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2019-09-27 18:25:15 -0700

    Updates MATESA to the version released on 2019-02-12. This is now reading the latest SNIP RIP MATESA correctly

--- pynitf/nitf_tre_matesa.py
+++ pynitf/nitf_tre_matesa.py
@@ -9,19 +9,28 @@ The field names can be pretty cryptic, but are documented in detail in
 the NITF TRE documentation (STDI-0002 V4.0, available at
 http://www.gwg.nga.mil/ntb/baseline/docs/stdi0002).
 
-MATESA is currently, Dec 2017, defined in the SNIP draft document.
-It will be finalized in sometime 2018
+MATESA v1.0 final v4 2019-02-12.
 '''
 
 desc = ["MATESA",
-        ["num_groups", "Number of Mate Relationship Types", 2, int],
+        ["cur_source", "Current File/Segment Source", 42, str],
+        ["cur_mate_type", "Current File/Segment Mate Type", 16, str],
+        ["cur_file_id_len", "Length of CUR FILE ID field", 4, int],
+        ["cur_file_id", "ID of the Current File/Segment", "f.cur_file_id_len", None,
+          {'field_value_class' : StringFieldData}],
+        ["num_groups", "Number of Mate Relationship Types", 4, int],
         [["loop", "f.num_groups"],
-         ["relationship", "Mate Relationship Type", 32, str],
-         ["num_mates", "Number of Mates", 3, int],
+         ["relationship", "Mate Relationship", 24, str],
+         ["num_mates", "Number of Mates", 4, int],
          [["loop", "f.num_mates[i1]"],
           ["source", "Mate Source", 42, str],
-          ["id_type", "Identifier Type", 20, str],
-          ["mate_id", "Mate File Identifier",256, str],
+          ["mate_type", "Identifier Type", 16, str],
+          ["mate_id_len", "Length of the mth MATE IDnm field", 4, int],
+
+          #TODO: This length doesn't seem to be enforced. Look into it later. It's working
+          #TODO: correctly otherwise.
+          ["mate_id", "Mate File Identifier","f.mate_id_len[i1, i2]", None,
+          {'field_value_class' : StringFieldData}],
          ],
         ],
 ]
