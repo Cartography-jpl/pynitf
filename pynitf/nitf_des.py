52f4cdc618d3b5c8cdacd153b961e9d2b5322596
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2020-04-06 16:07:47 -0700

    Merge NitfDesFieldStructObjectHandle and NitfDesFieldStruct, there is no reason to have them be seperate classes. We just key off of if des_implementation_field is defined, just like we handle objects with Tre.

--- pynitf/nitf_des.py
+++ pynitf/nitf_des.py
@@ -1,13 +1,4 @@
-# We import a number of "private" classes from nitf_field. This module really
-# is part of nitf_field.py, we have just pulled it out into a separate file
-# to keep things clean
-#
-# Note when importing TREs that much of the documentation is in PDF tables.
-# You can't easily paste this directly to emacs. But you can import to Excel.
-# To do this, cut and paste the table into *Word*, and then cut and paste
-# from word to Excel. For some reason, you can't go directly to Excel. You
-# can then cut and paste from excel to emacs
-from .nitf_field import FieldStruct
+from .nitf_field import FieldStruct, FieldStructDiff
 from .nitf_segment_data_handle import (NitfDes,
                                        NitfSegmentDataHandleSet)
 from .nitf_diff_handle import (NitfDiffHandle, NitfDiffHandleSet,
@@ -32,6 +23,12 @@ class NitfDesFieldStruct(NitfDes, FieldStruct):
     '''
     data_after_allowed = None
     data_copy = None
+    # If defined, we have the DES handled by an object type
+    # des_implementation_class in des_implementation_field.
+    # See for example DesCSATTB in geocal.
+    des_implementation_field = None
+    des_implementation_class = None
+    
     def __init__(self, seg=None):
         NitfDes.__init__(self, seg)
         FieldStruct.__init__(self)
@@ -39,13 +36,21 @@ class NitfDesFieldStruct(NitfDes, FieldStruct):
         self.data_after_tre_size = None
         self.data_to_copy = None
         
-    def read_from_file(self, fh, seg_index=None):
+    def read_from_file(self, fh, seg_index=None, force_raw_read=False):
         if(self.subheader.desid != self.des_tag):
             return False
+        if(self.des_implementation_field and not force_raw_read):
+            setattr(self, self.des_implementation_field,
+                    self.des_implementation_class.des_read(fh))
+            self.update_raw_field()
+            return True
         t = fh.tell()
         FieldStruct.read_from_file(self,fh)
         self.data_start = fh.tell()
-        self.data_after_tre_size = self._seg().data_size - (self.data_start - t)
+        if(self._seg is not None):
+            self.data_after_tre_size = self._seg().data_size - (self.data_start - t)
+        else:
+            self.data_after_tre_size = 0
         if(self.data_after_tre_size != 0):
             if(not self.data_after_allowed):
                 raise RuntimeError("DES %s TRE length was expected to be %d but was actually %d" % (self.des_tag, self._seg().data_size, (self.data_start - t)))
@@ -56,9 +61,12 @@ class NitfDesFieldStruct(NitfDes, FieldStruct):
         return True
         
     def write_to_file(self, fh):
-        FieldStruct.write_to_file(self, fh)
-        if(self.data_to_copy):
-            fh.write(self.data_to_copy)
+        if(self.des_implementation_field):
+            getattr(self, self.des_implementation_field).des_write(fh)
+        else:
+            FieldStruct.write_to_file(self, fh)
+            if(self.data_to_copy):
+                fh.write(self.data_to_copy)
             
     def str_hook(self, file):
         '''Convenient to have a place to add stuff in __str__ for derived
@@ -70,53 +78,20 @@ class NitfDesFieldStruct(NitfDes, FieldStruct):
     def __str__(self):
         '''Text description of structure, e.g., something you can print
         out.'''
-        res = io.StringIO()
-        self.str_hook(res)
-        if(self.user_subheader):
-            print("User-Defined Subheader: ", file=res)
-            print(self.user_subheader, file=res)
-        print(FieldStruct.__str__(self), file=res)
-        if(self.data_after_tre_size and self.data_after_tre_size != 0):
-            print("Extra data of length %d" % self.data_after_tre_size,
-                  file=res)
-        return res.getvalue()
-
-class NitfDesFieldStructObjectHandle(NitfDesFieldStruct):
-    '''This is a class where the DES is handled by an object type
-    des_implementation_class in des_implementation_field. This
-    possibly also include user defined subheaders.  See for example
-    DesCSATTB in geocal.
-    '''
-    def read_from_file(self, fh, seg_index=None):
-        if(DEBUG):
-            print("Trying to match %s" % self.subheader.desid)
-        if(self.subheader.desid != self.des_tag):
-            if(DEBUG):
-                print("Match failed")
-            return False
-        if(DEBUG):
-            print("Match succeeded in constructor")
-        setattr(self, self.des_implementation_field,
-                self.des_implementation_class.des_read(fh))
-        self.update_raw_field()
-        return True
-    
-    def write_to_file(self, fh):
-        getattr(self, self.des_implementation_field).des_write(fh)
-        
-    def str_hook(self, file):
-        '''Convenient to have a place to add stuff in __str__ for derived
-        classes. This gets called after the DES name is written, but before
-        any fields. Default is to do nothing, but derived classes can 
-        override this if desired.'''
-        pass
-    def __str__(self):
-        '''Text description of structure, e.g., something you can print
-        out.'''
-        if(self._seg and self._seg().nitf_file and
-           self._seg().nitf_file.report_raw):
+        if(not self.des_implementation_field or
+           (self._seg and self._seg().nitf_file and
+            self._seg().nitf_file.report_raw)):
             self.update_raw_field()
-            return super().__str__()
+            res = io.StringIO()
+            self.str_hook(res)
+            if(self.user_subheader):
+                print("User-Defined Subheader: ", file=res)
+                print(self.user_subheader, file=res)
+            print(FieldStruct.__str__(self), file=res)
+            if(self.data_after_tre_size and self.data_after_tre_size != 0):
+                print("Extra data of length %d" % self.data_after_tre_size,
+                      file=res)
+            return res.getvalue()
         res = io.StringIO()
         self.str_hook(res)
         if(self.user_subheader):
@@ -125,26 +100,36 @@ class NitfDesFieldStructObjectHandle(NitfDesFieldStruct):
         print("Object associated with DES:", file=res)
         print(getattr(self, self.des_implementation_field), file=res)
         return res.getvalue()
-    
+
     def update_raw_field(self):
         '''Update the raw fields after a change to des_implementation_field'''
         fh = io.BytesIO()
         self.write_to_file(fh)
         fh2 = io.BytesIO(fh.getvalue())
-        super().read_from_file(fh2)
-
-# TODO May want to rework this, not sure if having handle_diff in the
-# object is the right way to handle this.
-
-class DesFieldStructObjectDiff(NitfDiffHandle):
+        self.read_from_file(fh2, force_raw_read=True)
+    
+class DesFieldStructDiff(FieldStructDiff):
     '''Compare two NitfDesObjectHandle.'''
+    def configuration(self, nitf_diff):
+        return self._config
     def handle_diff(self, des1, des2, nitf_diff):
-        if(not isinstance(des1, NitfDesFieldStructObjectHandle) or
-           not isinstance(des2, NitfDesFieldStructObjectHandle)):
+        if(not isinstance(des1, NitfDesFieldStruct) or
+           not isinstance(des2, NitfDesFieldStruct)):
             return (False, None)
-        return (True, des1.handle_diff(des2))
+        if(des1.des_tag != des2.des_tag):
+            logger.difference("DES tags don't match. DES 1 '%s' and DES 2 '%s'",
+                              des.des_tag, des.des_tag)
+            return (True, False)
+        # If we have a handle_diff function use it. 
+        if(hasattr(des, "handle_diff")):
+            return (True, des1.handle_diff(des2))
+        # Otherwise compare the fields
+        self._config = nitf_diff.config.get("DES", {}).get(des1.des_tag, {})
+        with nitf_diff.diff_context("DES '%s'" % des1.des_tag, add_text = True):
+            return (True, self.compare_obj(des1, des2, nitf_diff))
 
-NitfDiffHandleSet.add_default_handle(DesFieldStructObjectDiff())
+NitfDiffHandleSet.add_default_handle(DesFieldStructDiff())
+NitfDiffHandleSet.default_config["DES"] = {}
     
 class NitfDesCopy(NitfDes):
     '''Implementation that reads from one file and just copies to the other.
@@ -196,19 +181,9 @@ class TreOverflow(NitfDes):
         out.'''
         return "TreOverflow"
 
-def _create_attribute_forward_to_implementation_object(fname):
-    def f(self):
-        t = getattr(self, self.des_implementation_field)
-        if(not hasattr(t, fname)):
-            raise AttributeError("The implementation class %s does not have the field %s" %(self.des_implementation_class.__name__, fname))
-        return getattr(t, fname)
-    return property(f)
-    
-
 NitfSegmentDataHandleSet.add_default_handle(TreOverflow)
 # Don't normally use, but you can add this if desired
 #NitfSegmentDataHandleSet.add_default_handle(NitfDesCopy, priority_order=-999)
 
-__all__ = [ "NitfDesCopy", "TreOverflow", "NitfDesFieldStruct",
-            "NitfDesFieldStructObjectHandle"]
+__all__ = [ "NitfDesCopy", "TreOverflow", "NitfDesFieldStruct",]
 
