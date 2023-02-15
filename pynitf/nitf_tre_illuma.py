d7ecc7afb1ab0868569594121f20863cfa6933e8
Brent Knickerbocker <brent.j.knickerbocker@jpl.nasa.gov>
2023-02-14 19:14:23 -0800

    Initial changes. Moved class with overrides into TRE file

--- pynitf/nitf_tre_illuma.py
+++ pynitf/nitf_tre_illuma.py
@@ -1,5 +1,8 @@
 from .nitf_tre import Tre, tre_tag_to_cls
 
+import xml.etree.ElementTree as ET
+import io
+
 hlp = '''This is the ILLUMA TRE, Illumination for Spectral Products. 
 
 The field names can be pretty cryptic, but are documented in detail in 
@@ -27,13 +30,64 @@ desc = [["sol_az", "Sun Azimuth Angle", 5, float, {'frmt': '%05.1f', 'optional':
         ["art_ill_max", "Maximum Artificial Illumination", 5, float, {'frmt': '%05.1f', 'optional': True, 'optional_char' : '-'}],
 ]
 
+
+# An example of implementing a nonstandard TRE. This depends on having
+# lxml available, so we don't have this in the main part of pynitf. But
+# we could add this, and in any case this is a nice example of using
+# a nonstandard TRE.
+
 class TreILLUMA(Tre):
     __doc__ = hlp
     desc = desc
     tre_tag = "ILLUMA"
+    field_list = ["solAz", "solEl", "comSolIl",
+                  "lunAz", "lunEl", "lunPhAng",
+                  "comLunIl", "solLunDisAd", "comTotNaIl",
+                  "artIlMin", "artIlMax"]
 
-#tre_tag_to_cls.add_cls(TreILLUMA)    
+    def __init__(self):
+        for f in self.field_list:
+            setattr(self, f, None)
 
-__all__ = [ ]
+    def tre_bytes(self):
+        # Can perhaps add in validation with the XLS. But for now, just
+        # do a simple xml file
+        res = b'<?xml version="1.0" encoding="UTF-8" ?>'
+        res += b"<ILLUMA>"
+        for f in self.field_list:
+            v = getattr(self, f)
+            if (v is not None):
+                res += f"  <{f}>{v}</{f}>".encode('utf-8')
+        res += b"</ILLUMA>"
+        return res
+
+    def read_from_tre_bytes(self, bt, nitf_literal=False):
+        root = ET.fromstring(bt)
+        for f in self.field_list:
+            n = root.find(f)
+            if (n is not None):
+                setattr(self, f, float(n.text))
+
+    def read_from_file(self, fh, delayed_read=False):
+        tag = fh.read(6).rstrip().decode("utf-8")
+        if (tag != self.tre_tag):
+            raise RuntimeError("Expected TRE %s but got %s" % (self.tre_tag, tag))
+        cel = int(fh.read(5))
+        self.read_from_tre_bytes(fh.read(cel))
+
+    def __str__(self):
+        '''Text description of structure, e.g., something you can print
+        out.'''
+        res = io.StringIO()
+        print("TRE - %s" % self.tre_tag, file=res)
+        for f in self.field_list:
+            print(f"{f}: {getattr(self, f)}", file=res)
+        return res.getvalue()
+
+
+tre_tag_to_cls.add_cls(TreILLUMA)
+
+
+__all__ = ["TreILLUMA", ]
 
 
